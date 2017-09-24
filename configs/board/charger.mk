bc03f8ff4156ba386f9589c7982ab7ac88afb008
deed821 <deed821@gmail.com>
2017-09-24 10:01:46 +1000

    hero: restore zero mk file as ramdisk references will work
    Change-Id: I0a185660e395a1c0a494a0dfc6af8f544193577f

--- configs/board/charger.mk
+++ configs/board/charger.mk
@@ -15,7 +15,8 @@
 #
 
 # Charger
-BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"BOARD_BATTERY_DEVICE_NAME := battery
+BACKLIGHT_PATH := /sys/devices/13900000.dsim/backlight/panel/brightness
+BOARD_BATTERY_DEVICE_NAME := battery
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
